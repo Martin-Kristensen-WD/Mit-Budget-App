69b49fe9895bab847924e0a7a7bb0b89bf1ee3c6
Martin Kristensen <matinndkk@hotmail.com>
2021-06-03 12:22:19 +0200

    added database connection

--- server/db.js
+++ server/db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool 
 
 const pool = new Pool({
-  user: "postgres",
-  password: "auv85qbd", 
-  host: "localhost", 
+  user: "ptbbvtjy",
+  password: "PGWO-C_5Ojtq51UNzG4c17_oxzxfJzOL", 
+  host: "tai.db.elephantsql.com", 
   port: 5432, 
-  database: "jwtloginsystem"
+  database: "ptbbvtjy"
 })
 
 module.exports = pool; 
+
